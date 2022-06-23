905c82fce0378b7d722c0c88cff694d70f5506b7
Soliu Adeosun <sawlew@gmail.com>
2022-06-23 17:45:10 +0100

    first commit

---
+++ src/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.urls import reverse,reverse_lazy
+from django.views import generic
+from .models import Post
+
+class PostListView(generic.ListView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(generic.UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
